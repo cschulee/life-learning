104d42ef74cafcd8f4af254d3a1babf546e2df6d
cschulee <christopherschulenberg@SchuMac.local>
2014-05-20 00:18:00 -0700

    Add new python module union
    Includes union function to take two lists and modify the first to be
    the union of both. Output is sorted.

---
+++ python/union.py
@@ -0,0 +1,11 @@
+# Takes two lists and modifies the first to be the union of both
+
+#union.union(list1,list2)
+def union(a,b):
+    
+    for i in b:
+        if  i not in a:
+            a.append(i)
+    a = a.sort
+        
+    return
